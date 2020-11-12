7136101d248c838f203c5f8aea760331334c3dd0
paul <p.patault@gmail.com>
2020-11-12 19:57:49 +0100

    Update main.ml

--- src/main.ml
+++ src/main.ml
@@ -11,19 +11,19 @@ let rec eval (fs: sequent) =
     Printf.printf "\n"; 
     let a = List.fold_left (fun acc e -> acc && eval e) true red_fs in
     decr decale; a
-  with | Loose -> Printf.printf "   → impossible\n"; false
-       | Win s -> Printf.printf "   → is axiom (%s)\n" s; true
+  with | Loose -> Printf.printf "   → impossible\n---- INVALIDE ----\n"; false
+       | Win s -> Printf.printf "   → is axiom (%s)\n---- VALIDE ----\n" s; true
 
 
 let make_formule (s: string) = 
-  Printf.printf "Formule de %s: " s; flush stdout;
+  Printf.printf "%s" s; flush stdout;
   let lexbuf = Lexing.from_channel stdin in
   let formule_in = Parser.main Lexer.scan_token lexbuf in
   formule_in
 
 let run () = 
-  let gauche = make_formule "gauche" in
-  let droite = make_formule "droite" in
+  let gauche = make_formule "Γ: " in
+  let droite = make_formule "À démontrer: " in
   Printf.printf "\n---- Évaluation ----\n"; flush stdout;
   let seq = { gauche = [gauche]; droite = [droite] } in
   eval seq
