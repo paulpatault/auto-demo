34fb7aa6cee23c7fc814ba4e8d32dd92fc366707
paul <p.patault@gmail.com>
2020-11-13 01:53:58 +0100

    maj printer (parenthesage optionel)

--- src/printer.ml
+++ src/printer.ml
@@ -1,5 +1,33 @@
 open Types
 
+let parenthesage = ref false 
+
+let () = 
+  try 
+    if int_of_string (Sys.argv.(1)) > 0 then parenthesage := true
+  with Invalid_argument s -> ()
+
+let red    = "\027[31m"
+let green  = "\027[32m"
+let orange = "\027[33m"
+let blue   = "\027[34m"
+let purple = "\027[35m"
+let cyan   = "\027[36m"
+let ltgrey = "\027[37m"
+let stop   = "\027[0m"
+
+let colors = [|blue; red; green; purple;|]
+let i = ref 0
+let len = Array.length colors
+
+let open_par i' =
+  incr i;
+  if !parenthesage then Printf.printf "%s" (colors.(i' mod len) ^ "(" ^ stop)
+
+let close_par i' =
+  if !parenthesage then Printf.printf "%s" (colors.(i' mod len) ^ ")" ^ stop)
+
+
 let rec formule_to_string (f: formule): unit =
   match f with
   | Vide -> Printf.printf "" 
@@ -11,13 +39,13 @@ let rec formule_to_string (f: formule): unit =
         Printf.printf "))"
       ) 
       else Printf.printf ")"
-  | Forall (v, f')   -> Printf.printf "pour tout %s " v; formule_to_string f'
-  | Exists (v, f')   -> Printf.printf "il existe %s " v; formule_to_string f'
-  | Or (f1, f2)      -> formule_to_string f1; Printf.printf " ∨ "; formule_to_string f2
-  | And (f1, f2)     -> formule_to_string f1; Printf.printf " ∧ "; formule_to_string f2
-  | Implies (f1, f2) -> formule_to_string f1; Printf.printf " ⇒ "; formule_to_string f2
-  | Not f'           -> Printf.printf "non "; formule_to_string f'
-  | Hyp lf           -> formules_to_string lf
+  | Forall (v, f')   -> let vi = !i in open_par vi; Printf.printf "pour tout %s " v; formule_to_string f'; close_par vi
+  | Exists (v, f')   -> let vi = !i in open_par vi; Printf.printf "il existe %s " v; formule_to_string f'; close_par vi
+  | Or (f1, f2)      -> let vi = !i in open_par vi; formule_to_string f1; Printf.printf " ∨ "; formule_to_string f2;close_par vi
+  | And (f1, f2)     -> let vi = !i in open_par vi; formule_to_string f1; Printf.printf " ∧ "; formule_to_string f2;close_par vi
+  | Implies (f1, f2) -> let vi = !i in open_par vi; formule_to_string f1; Printf.printf " ⇒ "; formule_to_string f2;close_par vi
+  | Not f'           -> let vi = !i in open_par vi; Printf.printf "non "; formule_to_string f';close_par vi
+  | Hyp lf           -> let vi = !i in open_par vi; formules_to_string lf; close_par vi
 and terme_to_string (t: terme): unit = 
   match t with 
   | Variable v -> Printf.printf "%s" v
